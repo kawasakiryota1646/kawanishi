1865bb9716d1cb1bf6fdb50ad25eb29f61489489
DESKTOP-NU7RLPI\GA3 <h1202419017@hiratagakuen.onmicrosoft.com>
2026-05-08 16:24:56 +0900

    no message

--- BAKUCHARI/Assets/1nakanishi/CameraController.cs
+++ BAKUCHARI/Assets/1nakanishi/CameraController.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // プレイヤー
     public GameObject player;
+
+    // 前回のプレイヤー位置
     Vector3 prePlayerPos;
 
     void Update()
     {
+        // プレイヤーの位置が変わったら
         if (player.transform.position != prePlayerPos)
         {
+            // カメラをプレイヤー位置に合わせる
             transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y + 1, -10);
+
+            // 現在のプレイヤー位置を保存
             prePlayerPos = player.transform.position;
         }
     }
