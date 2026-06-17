3089c49a2ed1d17be7cbc0394d7ed8987b1a1861
DESKTOP-HU9VP0K\GA3 <h1202419005@hiratagakuen.onmicrosoft.com>
2026-06-17 15:43:13 +0900

    no message

--- BAKUCHARI/Assets/1kawasaki/Script/StageSelect.cs
+++ BAKUCHARI/Assets/1kawasaki/Script/StageSelect.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 public class StageSelect : MonoBehaviour
 {
     [SerializeField] private string _loadScene; //シーン名を記述
-
+    public AudioSource audioSource;
+    public AudioClip clickSE;
     public void LoadStage(string stageName)
     {
+        audioSource.PlayOneShot(clickSE);
         SceneManager.LoadScene(stageName);
     }
 }
--- BAKUCHARI/Assets/1kawasaki/Script/TitleManager.cs
+++ BAKUCHARI/Assets/1kawasaki/Script/TitleManager.cs
@@ -7,9 +7,12 @@ public class TitleManager : MonoBehaviour
 {
 
     [SerializeField] private string _loadScene; //シーン名を記述
+    public AudioSource audioSource;
+    public AudioClip clickSE;
 
     public void LoadStage(string stageName)
     {
+        audioSource.PlayOneShot(clickSE);
         SceneManager.LoadScene(stageName);
         Destroy(GameObject.Find("BGMManager"));
 
